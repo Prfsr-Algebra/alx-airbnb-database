1e5c92864616be7ea51cbf5a8ee0342dc51359ad
Sulayman Muhammad <sulayman.m.f@gmail.com>
2025-07-07 03:40:34 +0530

    window and aggregate

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,3 +9,10 @@ RANK() OVER(
 FROM properties p
 LEFT JOIN bookings b ON b.property_id = p.property_id
 GROUP BY p.property_id, p.name
+SELECT p.property_id, p.name, b.booking_id, COUNT(b.booking_id),
+ROW_NUMBER() OVER(
+    ORDER BY COUNT(b.booking_id)
+) AS booking_rank
+FROM properties p
+LEFT JOIN bookings b ON b.property_id = p.property_id
+GROUP BY p.property_id, p.name
