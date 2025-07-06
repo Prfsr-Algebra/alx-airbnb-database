76e5b62a436678fd75abcaac3743319f005a625d
Sulayman Muhammad <sulayman.m.f@gmail.com>
2025-07-06 23:36:52 +0530

    joins quries

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,11 @@
+SELECT * 
+FROM bookings
+INNER JOIN user ON users.user_id = bookings.user_id;
+
+SELECT *
+FROM properties
+INNER JOIN reviews ON reviews.property_id = properties.property_id
+
+SELECT *
+FROM bookings
+FULL OUTER JOIN users ON users.user_id = bookings.user_id
