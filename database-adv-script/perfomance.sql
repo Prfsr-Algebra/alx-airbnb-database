d432c131b35a39b088d03bbc9953f2e4bee245f4
Sulayman Muhammad <sulayman.m.f@gmail.com>
2025-07-10 15:40:43 +0530

    optimization report

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -23,6 +23,8 @@ JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON pay.booking_id = b.booking_id;
 
+WHERE
+AND;
 
 EXPLAIN ANALYZE
 SELECT 
